6ab52b4354732ef0d37d2f6a27eb6358f1b9d297
Michael <michaeltesfayg@gmail.com>
2022-03-28 16:18:23 +0200

    keyboard event and two way data binding using vmodel

--- app.js
+++ app.js
@@ -2,40 +2,25 @@ new Vue({
 el:'#app',
 data(){
     return{
-        age:18,
-        EventModifiers:'<a href="https://vuejs.org/guide/essentials/event-handling.html#key-modifiers">Readmore about Event Modifiers</a>',
-        x:0,
-        y:0
+       name:"",
+       age:null,
+       title:"",
+       description:"",
+    EventModifiers:'<a href="https://vuejs.org/guide/essentials/event-handling.html#key-modifiers">Readmore about Event Modifiers</a>',
+
         
         
     }
 },
 methods:{
-    add(){
-        if(this.age<100){
-            return this.age+=1;
-        }else{
-            return this.age="101 Years? Are you an Aline?"
-        }
+    logName(){
+
+    console.log("Enter key ditected for name input")
      
     },
-    minus(){
-        if(this.age>0){
-            this.age-=1
-        }
-       
-    },
-    doubleCurrent(){
-       return this.age*=2
-    },
-    resetAge(){
-        return this.age=18
-    },
-    setXY(e){
-
-        this.x=e.offsetX;
-        this.y=e.offsetY;
+   logAge(){
+    console.log("Ctrl + enter key ditected for age input")
     }
-    
+  
 }
 })
